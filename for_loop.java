380fd050283b807e1335335dff902499dc266ad1
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-05-02 21:23:39 +0530

    Add files via upload

---
+++ for_loop.java
@@ -0,0 +1,7 @@
+public class for_loop {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 10; i++){
+            System.out.println("Hello World...!1");
+        }
+    }    
+}
